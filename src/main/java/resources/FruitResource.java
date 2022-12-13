8f49b991cd2d0e4fa3548f98d1704b5868f595aa
Igor Neves Faustino <igor@nfaustino.com>
2022-12-13 16:19:19 -0300

    refactor: use Response type

--- src/main/java/resources/FruitResource.java
+++ src/main/java/resources/FruitResource.java
@@ -1,7 +1,6 @@
 package resources;
 
 import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
 
 import javax.ws.rs.DELETE;
@@ -9,6 +8,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 import domain.entities.Fruit;
 import domain.repositories.FruitRepository;
@@ -25,24 +25,27 @@ public class FruitResource {
     }
 
     @GET
-    public Set<Fruit> list() {
-        return repository.getAll();
+    public Response list() {
+        return Response.ok(repository.getAll()).build();
     }
 
     @GET
     @Path("/{id}")
-    public Optional<Fruit> getById(@PathParam("id") UUID id) {
-        return repository.getById(id);
+    public Response getById(@PathParam("id") UUID id) {
+        Optional<Fruit> fruit = repository.getById(id);
+        if (fruit.isEmpty())
+            return Response.noContent().status(404).build();
+        return Response.ok(fruit.get()).build();
     }
 
     @POST
-    public Fruit add(Fruit fruit) {
-        return repository.add(fruit);
+    public Response add(Fruit fruit) {
+        return Response.ok(repository.add(fruit)).status(201).build();
     }
 
     @DELETE
     @Path("/{id}")
-    public Boolean delete(@PathParam("id") UUID id) {
-        return repository.delete(id);
+    public Response delete(@PathParam("id") UUID id) {
+        return Response.ok(repository.delete(id)).build();
     }
 }
